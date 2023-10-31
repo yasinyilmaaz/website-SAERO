8e51f3a66e5c7c0f4ba9539439c7333766d2d062
yasin <yasn.866644@gmail.com>
2023-10-31 16:05:05 +0300

    navbarı bölümlere yırdım ve küçük bir başlangıç yaptım

--- src/Components/navbar/Navbar1.js
+++ src/Components/navbar/Navbar1.js
@@ -1,5 +1,23 @@
-export default function Navbar1(){
-    return(
-        <div>Navbar1</div>
-    )
+import { Container } from "react-bootstrap";
+
+export default function Navbar1() {
+  return (
+    <nav className="h-[60px] bg-[#000000f5] text-white navbar">
+      <Container className="flex justify-between items-center">
+        <div className="flex items-center flex-row">
+        <div></div>
+          <a href="#" className="h-8">
+          <figure >
+            <img
+              src="https://tua.gov.tr/images/tua-logo-tr.svg"
+              alt="Saerotech"
+              className="h-8"
+            />
+          </figure>
+          </a>
+        </div>
+        <div>degerler</div>
+      </Container>
+    </nav>
+  );
 }
