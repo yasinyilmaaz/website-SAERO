39928ee4ae216deac8347459ac1b2b7ba9fcc7ee
ABDOUL FARIDE BASSIROU ALZOUMA <abdoulfaridbassirou7898@gmail.com>
2023-11-01 17:52:00 +0300

    tab content created

--- src/Components/tabss/tabs.js
+++ src/Components/tabss/tabs.js
@@ -2,32 +2,39 @@ import { Button, Container } from "react-bootstrap";
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 
-export default function Breadcrumb() {
+export default function tabbs() {
   return (
     <div>
     <Tabs
       defaultActiveKey="home"
       id="justify-tab-example"
-      className="bg-[#000000f5] text-white navbar mb-3"
+      className="bg-[#000000f5] text-white"
       justify
     >
       <Tab eventKey="home" title="00 Ana Sayfa">
-               
-     görsel component
-     
-
+          <div className="tabb">
+                Tab content for Ana Sayfa
+          </div>
       </Tab>
-      <Tab eventKey="kurumsal" title="01 Kurumsal">
-        gdf
+      <Tab  eventKey="kurumsal" title="01 Kurumsal">
+                  <div className="tabb">
+                Tab content for kurumsal
+                  </div>
       </Tab>
       <Tab eventKey="projeler" title="02 Projeler">
-        gdfg
+                 <div className="tabb">
+                Tab content for Projeler
+                 </div>
       </Tab>
       <Tab eventKey="egitim" title="03 Eğitim ve Sosyal">
-        gdf
+                  <div className="tabb">
+                Tab content for Eğitim
+                   </div>
       </Tab>
       <Tab eventKey="ulusal" title="04 ulusal ve uluslararası">
-        gdf
+                  <div className="tabb">
+                Tab content for ulusal ve uluslararası
+                  </div>
       </Tab>
     </Tabs>
     </div>
