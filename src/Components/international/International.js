40e66af376efa8514cc329ca76a8f7fe8da7d668
yasin <yasn.866644@gmail.com>
2023-11-04 16:44:33 +0300

    birkaç sayfayı düzenledim

--- src/Components/international/International.js
+++ src/Components/international/International.js
@@ -1,9 +1,23 @@
-import NavbarInstiutional from "../navbar/NavbarInstiutional";
+import { Container } from "react-bootstrap"
+
 
 export default function International(){
     return(
-        <div>
-           gfdg
+        <div className="banner-home">
+      <Container className="">
+        <div className="banner-content">
+          <h1>Uluslararası Faaliyetler</h1>
+          <p>
+          Türkiye Uzay Ajansı, uzay ve havacılıkla ilgili uluslararası kuruluşlarda Türkiye’yi temsil etmek ve bu kuruluşlarla ilgili faaliyetleri yürütmekle görevlidir.
+          </p>
         </div>
+        <div className="flex flex-col gap-4">
+          <div className="text-white">
+            
+          </div>
+          <hr className="h-1 border-t-2 border-t-white" />
+        </div>
+      </Container>
+    </div>
     )
 }
